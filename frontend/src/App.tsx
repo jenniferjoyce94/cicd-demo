7df274118046d29b1d60c0eee0505e8775c73127
jenniferjoyce94 <jennifer.joyce.jaglund@gmail.com>
2025-11-20 10:49:46 +0100

    test 5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <div>
         <p>{secret}</p>
         <h1>DevOps Demo</h1>
+        <h2>twatttttt</h2>
         <h2>
           {" "}
           Ni ska sätta upp CI/CD för. Pipen ska köra tester och bygga projektet
